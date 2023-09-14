288a04c78042a25291637a4c03af3d8eb572ed78
dongxihang <xihang.dong@amicro.com.cn>
2023-09-14 11:07:13 +0800

    add ufo neovim for fold code & keymaps

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -10,3 +10,9 @@ keymap.set("i", "jk", "<ESC>")
 keymap.set("n", "<C-Left>", ":bnext<cr>")
 keymap.set("n", "<C-Right>", ":bprevious<cr>")
 
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
+keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+
+-- 取消高亮
+keymap.set("n", "<leader>nh", ":nohl<CR>")
